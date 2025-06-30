bfe8768695a94a05324cfd8668bd551c511c486e
Poorvik P <poorvikp508@gmail.com>
2025-06-30 22:43:11 +0530

    home page done

---
+++ src/components/TitileCard/TitileCard.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react'
+import './TitileCard.css'
+import cards_data from '../../assets/cards/cards_data'
+
+const TitileCard = () => {
+  const cardsRef = useRef();
+  const handlewheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLef += event.deltaY;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handlewheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            {/* <p>{card.name}</p> */}
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TitileCard
